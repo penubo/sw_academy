308dbf50e1b2af38264c1e0846240ea48fc137f8
yongkim <penubokim@gmail.com>
2020-05-30 12:02:47 +0900

    permutation 1 done in c++

---
+++ 9940-permutation1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+int main() {
+
+	int tt;
+
+	cin >> tt;
+
+	for (int t = 1; t <= tt; t++) {
+		int n, x;
+		cin >> n;
+		bool visited[n+1];
+		bool flag = true;
+		memset(visited, 0, n+1);
+
+		for (int i = 0; i < n; i++) {
+			cin >> x;
+			if (visited[x])
+				flag = false;
+			visited[x] = true;
+		}
+
+		if (flag)
+			cout << "#" << t << " " << "Yes\n";
+		else
+			cout << "#" << t << " " << "No\n";
+	}
+
+	return 0;
+}
